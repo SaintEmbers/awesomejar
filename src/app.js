2b05f7fe91a5075a3a132b88c27898d2ad971c3c
SaintEmbers <quantum108@gmail.com>
2016-01-19 20:13:52 -0800

    Delete files from relationplex
    I brokeup what was originally awesomejar and relationplex into two separate apps.

--- src/app.js
+++ src/app.js
@@ -4,18 +4,14 @@ require ('bootstrap/dist/css/bootstrap.css')
 require('./styles/main.css')
 import InputAwesome from './submit.js'
 import Jar from './components/eventsjar.js'
-import InputFocus from './components/focusApp.js'
-import Goals from './components/goals.js'
 
 require('./styles/main.css')
 var App  = React.createClass({
 
   render (){
     return <div>
-      <InputFocus/>
       <InputAwesome/>
       <Jar/>
-      <Goals/>
     </div>
   }
 })
